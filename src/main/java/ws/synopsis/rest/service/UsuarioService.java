d05f67216dc695a0e09575b7c872656bffe953ab
ArtNL <arturofnl@outlook.com>
2021-11-30 13:02:56 -0500

    Implementacion del Service

--- src/main/java/ws/synopsis/rest/service/UsuarioService.java
+++ src/main/java/ws/synopsis/rest/service/UsuarioService.java
@@ -2,13 +2,14 @@ package ws.synopsis.rest.service;
 
 import java.util.List;
 
+import ws.synopsis.rest.bean.request.UsuarioRequest;
 import ws.synopsis.rest.model.Usuario;
 
 public interface UsuarioService {
 
 	public List<Usuario> listarUsuarios();
-	public boolean add(Usuario usuario);
-	public boolean update(int idUsuario,Usuario usuario);
+	public boolean add(UsuarioRequest usuario);
+	public boolean update(int idUsuario,UsuarioRequest usuario);
 	public boolean delete(int idUsuario);
 	
 }
--- src/main/java/ws/synopsis/rest/service/impl/UsuarioServiceImpl.java
+++ src/main/java/ws/synopsis/rest/service/impl/UsuarioServiceImpl.java
@@ -1,5 +1,49 @@
 package ws.synopsis.rest.service.impl;
 
-public class UsuarioServiceImpl {
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import ws.synopsis.rest.bean.request.UsuarioRequest;
+import ws.synopsis.rest.model.Usuario;
+import ws.synopsis.rest.repository.UsuarioRepository;
+import ws.synopsis.rest.service.UsuarioService;
+
+public class UsuarioServiceImpl implements UsuarioService{
+
+	private UsuarioRepository usuarioRepository;
+
+	@Override
+	public List<Usuario> listarUsuarios() {
+		
+		return usuarioRepository.listaUsuarios();
+	}
+
+	@Override
+	public boolean add(UsuarioRequest usuario) {
+		return usuarioRepository.add(Usuario.builder()
+				.nombre(usuario.getNombre())
+				.apellido(usuario.getApellido())
+				.celular(usuario.getCelular())
+				.contraseña(usuario.getContraseña()).build());
+		
+	}
+
+	@Override
+	public boolean update(int idUsuario, UsuarioRequest usuario) {
+		return usuarioRepository.update(idUsuario, Usuario.builder()
+				.nombre(usuario.getNombre())
+				.apellido(usuario.getApellido())
+				.celular(usuario.getCelular())
+				.contraseña(usuario.getContraseña()).build());
+		
+	}
+
+	@Override
+	public boolean delete(int idUsuario) {
+		return usuarioRepository.delete(idUsuario);
+		
+	}
+	
 
 }
